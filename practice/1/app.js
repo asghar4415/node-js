221d6bd474ea1fc2232af4e1a709a70716be517d
Asghar Ali <asgharali.224415@gmail.com>
2024-04-18 21:50:32 +0500

    updated

---
+++ practice/1/app.js
@@ -0,0 +1,63 @@
+//node js
+//- nodejs is a runtime environment for executing javascript code. it is built on the v8 javascript engine and it is used to build fast and scalable network applications. it is written in c++ and javascript.
+//- nodejs is a cross-platform environment that runs on windows, macos, and linux operating systems.
+
+//nodejs is used for:
+//- web development (server-side)
+//- networking applications
+//- real-time chat applications
+//- command-line applications
+//- REST API server
+
+
+var a=10;
+var b=20;
+var c=a+b;
+console.log("sum of a and b is : "+c);
+//run this code through command prompt by using (node app.js) command
+
+//what is a server?
+//- a server is a computer program that provides services to other computer programs in the same or other computers. the computer that a server program runs in is also frequently referred to as a server.
+//- servers are used to manage network resources, share information, or run services for other computers.
+//- servers are often dedicated, meaning that they perform no other tasks besides their server tasks.
+
+//services provided by servers:
+//- file server
+//- database server
+//- mail server
+//- web server
+//- application server
+//- proxy server
+
+//what is a server-side language?
+//- a server-side language is a programming language that runs on the web server. it is used to create dynamic web pages and web applications.
+//- server-side languages are used to interact with the server, database, and other services.
+//- some popular server-side languages are:
+//- php
+//- python
+//- java
+//- ruby
+//- nodejs
+//- asp.net
+//- perl
+
+
+
+//what is a client?
+//- a client is a piece of computer hardware or software that accesses a service made available by a server as part of the client-server model of computing.
+//- the server is often on another computer system, in which case the client accesses the service by way of a network.
+//- the term applies to the role that programs or devices play in the client-server model.
+
+//what is a client-side language?
+//- a client-side language is a programming language that runs on the client-side (web browser). it is used to create interactive web pages and web applications.
+//- client-side languages are used to interact with the user, browser, and other services.
+//- some popular client-side languages are:
+//- html
+//- css
+//- javascript
+//- jquery
+//- angular
+//- react
+
+
+
